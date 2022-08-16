ce337f267d628f9afc27192b90b0d9cb5ced5bd0
Elli Saarenmaa <47243975+ellisrnm@users.noreply.github.com>
2022-08-16 21:49:27 +0300

    Prime number generation

--- rsa/key.py
+++ rsa/key.py
@@ -1,6 +1,160 @@
+"""Salausavainten luominen"""
+
+import random
+
 class Key:
+    """
+    Sisältää metodit salausavainten luomiseen.
+    """
+
     def __init__(self):
-        pass
+        """
+        Luokan konstruktori
+        """
+
+    def generate_prime_candidate(self, bits: int):
+        """
+        Palauttaa n-bitin mittaisen suuren satunnaisen luvun. Suuri luku valitaan
+        siten, että ensimmäisen bitin tulee olla 1 ja luvun tulee olla pariton.
+
+        Args:
+            bits (int): Haluttu bittien lukumäärä n
+
+        Returns:
+            Satunnaisluku väliltä 2^(n-1)+1 ja 2^n-1
+        """
+        candidate = 2
+        while candidate % 2 == 0:
+            candidate = random.randrange(2**(bits-1)+1, 2**bits-1)
+        return candidate
+
+    def sieve_of_eratosthenes(self, max_value: int):
+        """
+        Palauttaa listan niistä alkuluvuista, jotka ovat pienempiä tai yhtäsuuria kuin max_value.
+        Määrittää alkuluvut käyttäen Eratostheneen seulaa.
+
+        Args:
+            max_value (int): Haluttu yläraja, jota pienemmät alkuluvut haetaan
+
+        Returns:
+            Palauttaa listan alkulukuja
+        """
+        primes = [i>1 for i in range(max_value+1)]
+        i = 2
+        while i*i <= max_value:
+            if primes[i] is True:
+                for j in range(i**2, max_value+1, i):
+                    primes[j] = False
+            i += 1
+        return [i for i, x in enumerate(primes) if x]
+
+    def low_level_primality_test(self, prime_candidate: int, max_value: int=5000):
+        """
+        Nopea alkulukutesti, jossa valittu luku jaetaan muilla alkuluvuilla
+        jaollisuuden tarkistamiksi. Jos valittu luku on jaollinen testatuilla
+        alkuluvuilla, testi epäonnistuu.
+
+        Args:
+            prime_candidate (int): Testattavaksi valittu luku
+            max_value (int, optional): Yläraja, jota pienemmillä alkuluvuilla jaollisuus testataan
+                                       Oletusarvo 5000
+
+        Returns:
+            True, jos luku ei ole jaollinen testatuilla alkuluvuilla
+            False, jos luku on jaollinen testatuilla alkuluvuilla
+        """
+        primes_list = self.sieve_of_eratosthenes(max_value)
+        for div in primes_list:
+            if prime_candidate % div == 0 and div**2 <= prime_candidate:
+                return False
+        return True
+
+    def miller_rabin_round(self, prime_candidate: int, even_comp: int, divs: int):
+        """
+        Määrittää Mille-Rabin-algoritmin mukaisen yhden kierroksen avulla, onko tietty
+        luku todennäköisesti alkuluku. Testattavaksi valittu luku annetaan muodossa
+        prime_candidate = 2^divs*even_comp+1
+
+        Args:
+            prime_candidate (int): Testattavaksi valittu luku
+            even_comp (int): Testavaksi valitun luvun osa muodossa 2^divs*even_comp+1
+            divs (int): Testavaksi valitun luvun osa muodossa 2^divs*even_comp+1
+
+        Returns:
+            True, jos n on todennäköisesti alkuluku
+            False, jos n on yhdistelmäluku (ei alkuluku)
+        """
+        rand_val = random.randrange(2, prime_candidate-2)
+        result = pow(rand_val, even_comp, prime_candidate)
+        if result in (1, prime_candidate-1):
+            return True
+        for _ in range(divs):
+            result = pow(result, 2, prime_candidate)
+            if result == prime_candidate-1:
+                return True
+            if result == 1:
+                return False
+        return False
+
+    def miller_rabin_primality_test(self, prime_candidate: int, rounds: int=5):
+        """
+        Määrittää Mille-Rabin-algoritmin mukaan onko tietty luku todennäköisesti alkuluku.
+        Testikierroksia voidaan suorittaa useampia.
+
+        Args:
+            prime_candidate (int): Testattavaksi valittu luku
+            rounds (int, optional): Testikierrosten lukumäärä, oletusarvo 5
+
+        Returns:
+            True, jos n on todennäköisesti alkuluku
+            False, jos n on yhdistelmäluku (ei alkuluku)
+        """
+        even_comp = prime_candidate - 1
+        divs = 0
+        while even_comp % 2 == 0:
+            even_comp >>= 1
+            divs += 1
+        for _ in range(rounds):
+            if not self.miller_rabin_round(prime_candidate, even_comp, divs):
+                return False
+        return True
+
+    def generate_prime(self, bits):
+        """
+        Generoi satunnaisia lukuja ja testaa onko luku alkuluku niin kauan, että todennäköinen
+        alkuluku löytyy. Palauttaa n-bitin mittaisen satunnaisen (todennäköisen) alkuluvun.
+
+        Args:
+            bits (int): Haluttu bittien lukumäärä n
+
+        Returns:
+            Todennäköinen alkuluku
+        """
+        while True:
+            prime_candidate = self.generate_prime_candidate(bits)
+            if self.low_level_primality_test(prime_candidate):
+                if self.miller_rabin_primality_test(prime_candidate):
+                    return prime_candidate
+
+    def generate_primes(self, bits: int=1024, no_primes: int=2):
+        """
+        Palauttaa listan, jossa on n-bitin mittaisia satunnaisia (todennäköisiä)
+        alkulukuja m kappaletta.
+
+        Args:
+            bits (int, optional): Haluttu bittien lukumäärä n, oletusarvo 1024
+            no_primes (int, optional): Halutun listan pituus m, oletusarvo 2
+
+        Returns:
+            Palauttaa listan satunnaisia alkulukuja
+        """
+        primes = []
+        for _ in range(no_primes):
+            primes.append(self.generate_prime(bits))
+        return primes
 
     def generate_key(self):
+        """
+        Keskeneräinen
+        """
         return 'ABC'
